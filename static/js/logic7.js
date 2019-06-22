329d8582c92bda1d6e994c464bf25767114a44bd
tolu1111 <toluolayemi@gmail.com>
2019-06-22 00:02:47 -0500

    clean directory

--- static/js/logic7.js
+++ static/js/logic7.js
@@ -40,7 +40,7 @@ function createMap(mlbStadiums) {
 
   // Create the map object with options
   var map = L.map("map", {
-    center: [34.82, -98.57],
+    center: [39.82, -98.57],
     zoom: 5.2,
     layers: [lightmap, streetmap, darkmap, mlbStadiums]
   });
